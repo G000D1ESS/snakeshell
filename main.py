0f193e4d365612ecce6f4dd0a4b7f7c7e604bfe1
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-02-10 01:15:24 +0200

    add basic shell commands support

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import os
+import sys
+
+from utils import getcmd, change_dir
 
 
 def main():
-    ...
+    sys.tracebacklimit = 0
+    while True:
+        cmd, args = getcmd() 
+        if cmd == 'cd':
+            change_dir(args[1])
+            continue
+        pid = os.fork()
+        if pid == 0:
+            os.fsync(0)
+            os.execv(f'/bin/{args[0]}', args)
+        else:
+            _, status = os.waitpid(pid, os.P_WAIT)
+            if status != 0:
+                os.write(1, b'Exit: %d\n' % status)
+
 
 if __name__ == '__main__':
     main()
---
+++ utils.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+
+def promt() -> str:
+    """
+    Generete promt message.
+    """
+    dirname = os.getcwd()
+    homedir = os.path.expanduser('~')
+    if dirname.startswith(homedir):
+        dirname = '~' + dirname[len(homedir):]
+    return f'{dirname} $ '
+
+
+def change_dir(path: str) -> None:
+    """
+    Move to directory.
+    """
+    path = os.path.normpath(path)
+    path = os.path.expanduser(path)
+    os.chdir(path)
+
+
+def parse(cmd: str) -> tuple[str, list[str]]:
+    """
+    Parse command line.
+    """
+    args = cmd.strip().split(' ')
+    return args[0], args
+
+
+def getcmd() -> tuple[str, list[str]]:
+    """
+    Request user to write command, then parse it.
+    """
+    msg = promt()
+    os.write(1, msg.encode())
+    cmd = os.read(0, 1024).decode()
+    return parse(cmd)
+
