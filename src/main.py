d1b58e9c114847c72647d9bfa325f4880116ddab
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-02-10 21:51:40 +0200

    add sigint support

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import os
 import sys
+import signal
 
 from src.utils import getcmd, change_dir
 
 
 def main():
+
+    # Setup 
     sys.tracebacklimit = 0
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    # Main loop
     while True:
         cmd, args = getcmd()
         if not cmd:
@@ -18,6 +24,7 @@ def main():
         pid = os.fork()
         if pid == 0:
             os.fsync(0)
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
             os.execvp(cmd, args)
         else:
             _, status = os.wait()
