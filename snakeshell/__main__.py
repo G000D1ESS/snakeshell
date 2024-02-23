90e7edf625af2357c3aadf60ce8463a6142f0870
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-02-23 22:06:46 +0200

    refactor main.py and rename it

--- snakeshell/__main__.py
+++ snakeshell/__main__.py
@@ -1,6 +1,40 @@
-from .main import main
+import os
+import sys
+import signal
+
+from .utils import getcmd, change_dir
+
+
+def setup():
+    sys.tracebacklimit = 0
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def loop():
+    while True:
+        command = getcmd()
+        if not command:
+            continue
+        match command.path:
+            case 'cd':
+                change_dir(command.args[1])
+            case 'exit':
+                break
+            case _:
+                pid = os.fork()
+                if pid == 0:
+                    command.run()
+                # Wait child process
+                _, status = os.wait()
+                if status != 0:
+                    os.write(2, b'\033[101mExit: %d\033[0m\n' % status)
+
+
+def run_shell():
+    setup()
+    loop()
 
 
 if __name__ == '__main__':
-    main()
+    run_shell()
 
