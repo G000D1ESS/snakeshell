09852b912f5cdb5a7641e8e67eefb3e264f5acb8
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-02-23 22:52:52 +0200

    add new function to run command (support redirects)

--- snakeshell/cmd.py
+++ snakeshell/cmd.py
@@ -23,3 +23,33 @@ class Command:
             args=[self.path] + self.args,
         )
 
+
+def run_command(
+    cmd: Command,
+    write_to: str | None = None,
+    read_from: str | None = None,
+) -> None:
+
+    # Redirect standard output if `write_to` is specified.
+    if write_to:
+        os.close(1)
+        os.open(
+            mode=0o644,
+            path=write_to,
+            flags=os.O_WRONLY|os.O_CREAT|os.O_TRUNC,
+        )
+        os.set_inheritable(1, True)
+
+    # Redirect standard input if `read_from` is specified.
+    if read_from:
+        os.close(0)
+        os.open(
+            mode=0o644,
+            path=read_from,
+            flags=os.O_RDONLY,
+        )
+        os.set_inheritable(0, True)
+
+    # Execute the command with optional redirections applied.
+    cmd.run()
+
