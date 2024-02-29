b694f9e6ae46af74c3552670f81344bc2f720a4a
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-02-29 21:00:20 +0200

    hide shell logic in shell.py

--- snakeshell/__main__.py
+++ snakeshell/__main__.py
@@ -1,37 +1,4 @@
-import sys
-import signal
-
-from .console import error as show_error 
-from .console import prompt_user_to_input
-from .parser import parse
-
-
-def setup():
-    # Disable the traceback to simplify error messages.
-    sys.tracebacklimit = 0
-
-    # Ignore SIGINT (Ctrl+C), preventing the shell from exiting on Ctrl+C.
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-
-def loop():
-    # Start an infinite loop to continuously accept commands.
-    while True:
-        line = prompt_user_to_input()
-        if not line:
-            break
-        line = line.strip()
-        if not line:
-            continue
-        command = parse(line)
-        exit_code = command.execute()
-        if exit_code != 0:
-            show_error(f'Exit: {exit_code}')
-
-
-def run_shell():
-    setup()
-    loop()
+from .shell import run_shell
 
 
 if __name__ == '__main__':
---
+++ snakeshell/shell.py
@@ -0,0 +1,35 @@
+import sys
+import signal
+
+from .console import error as show_error 
+from .console import prompt_user_to_input
+from .parser import parse
+
+
+def setup():
+    # Disable the traceback to simplify error messages.
+    sys.tracebacklimit = 0
+
+    # Ignore SIGINT (Ctrl+C), preventing the shell from exiting on Ctrl+C.
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def loop():
+    # Start an infinite loop to continuously accept commands.
+    while True:
+        line = prompt_user_to_input()
+        if not line:
+            break
+        line = line.strip()
+        if not line:
+            continue
+        command = parse(line)
+        exit_code = command.execute()
+        if exit_code != 0:
+            show_error(f'Exit: {exit_code}')
+
+
+def run_shell():
+    setup()
+    loop()
+
