ddc976bbef6b7fd8da33f2789a99edd04cf6cc44
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-04-27 23:29:59 +0300

    reformat tests structure

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,14 +7,33 @@ from snakeshell.tree import CommandNode
 @pytest.mark.parametrize(
     "command, excpected_path, excpected_args",
     [
-        ['/bin/echo', '/bin/echo', ['/bin/echo']],
-        ['/bin/echo foo bar', '/bin/echo', ['/bin/echo', 'foo', 'bar']],
-        ["/bin/echo 'foo bar'", '/bin/echo', ['/bin/echo', 'foo bar']],
-        ['/bin/echo "foo bar"', '/bin/echo', ['/bin/echo', 'foo bar']],
-        ['/bin/echo \'a b\' "c d"', '/bin/echo', ['/bin/echo', 'a b', 'c d']],
+        [
+            '/bin/echo',
+            '/bin/echo', ['/bin/echo'],
+        ],
+        [
+            '/bin/echo foo bar',
+            '/bin/echo', ['/bin/echo', 'foo', 'bar'],
+        ],
+        [
+            "/bin/echo 'foo bar'",
+            '/bin/echo', ['/bin/echo', 'foo bar'],
+        ],
+        [
+            '/bin/echo "foo bar"',
+            '/bin/echo', ['/bin/echo', 'foo bar'],
+        ],
+        [
+            '/bin/echo \'a b\' "c d"',
+            '/bin/echo', ['/bin/echo', 'a b', 'c d'],
+        ],
     ],
 )
-def test_command_parsing(command, excpected_path, excpected_args):
+def test_command_parsing(
+    command,
+    excpected_path,
+    excpected_args,
+):
     result = parse(command)
     assert isinstance(result, CommandNode)
     assert result.arguments == excpected_args
