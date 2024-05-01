310952523036e22aaddccadcd52bafe91e723cf3
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-05-01 17:25:54 +0300

    back to test class structure

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,38 +4,76 @@ from snakeshell.parser import parse
 from snakeshell.tree import CommandNode
 
 
-@pytest.mark.parametrize(
-    "command, excpected_path, excpected_args",
-    [
-        [
-            '/bin/echo',
-            '/bin/echo', ['/bin/echo'],
-        ],
-        [
-            '/bin/echo foo bar',
-            '/bin/echo', ['/bin/echo', 'foo', 'bar'],
-        ],
-        [
-            "/bin/echo 'foo bar'",
-            '/bin/echo', ['/bin/echo', 'foo bar'],
-        ],
-        [
-            '/bin/echo "foo bar"',
-            '/bin/echo', ['/bin/echo', 'foo bar'],
-        ],
-        [
-            '/bin/echo \'a b\' "c d"',
-            '/bin/echo', ['/bin/echo', 'a b', 'c d'],
-        ],
-    ],
-)
-def test_command_parsing(
-    command,
-    excpected_path,
-    excpected_args,
-):
-    result = parse(command)
-    assert isinstance(result, CommandNode)
-    assert result.arguments == excpected_args
-    assert result.execute_path == excpected_path
+class TestCommandParse:
+
+    @staticmethod
+    def is_command_node(obj):
+        return type(obj) == CommandNode
+
+    @staticmethod
+    def node_to_dict(obj):
+        return {
+            'exec': obj.execute_path,
+            'args': obj.arguments,
+        }
+
+    def test_without_arguments(self):
+        command = '/bin/echo'
+
+        node = parse(command)
+        node_as_dict = self.node_to_dict(node)
+
+        assert self.is_command_node(node)
+        assert node_as_dict == {
+            'exec': '/bin/echo',
+            'args': ['/bin/echo'],
+        }
+
+    def test_with_arguments(self):
+        command = '/bin/echo a b c'
+
+        node = parse(command)
+        node_as_dict = self.node_to_dict(node)
+
+        assert self.is_command_node(node)
+        assert node_as_dict == {
+            'exec': '/bin/echo',
+            'args': ['/bin/echo', 'a', 'b', 'c'],
+        }
+
+    def test_with_single_quoted_argument(self):
+        command = "/bin/echo 'a b c'"
+
+        node = parse(command)
+        node_as_dict = self.node_to_dict(node)
+
+        assert self.is_command_node(node)
+        assert node_as_dict == {
+            'exec': '/bin/echo',
+            'args': ['/bin/echo', 'a b c'],
+        }
+
+    def test_with_double_quoted_argument(self):
+        command = '/bin/echo "a b c"'
+
+        node = parse(command)
+        node_as_dict = self.node_to_dict(node)
+
+        assert self.is_command_node(node)
+        assert node_as_dict == {
+            'exec': '/bin/echo',
+            'args': ['/bin/echo', 'a b c'],
+        }
+
+    def test_with_double_and_single_quoted_arguments(self):
+        command = '/bin/echo "first one" \'second one\''
+
+        node = parse(command)
+        node_as_dict = self.node_to_dict(node)
+
+        assert self.is_command_node(node)
+        assert node_as_dict == {
+            'exec': '/bin/echo',
+            'args': ['/bin/echo', 'first one', 'second one'],
+        }
 
