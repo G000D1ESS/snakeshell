5d5b5a2d5f7bc97cabf86599fed701e0e140e954
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-05-01 17:31:43 +0300

    split parser tests to diffrents modules

---
+++ tests/parser/test_command_parse.py
@@ -0,0 +1,81 @@
+import pytest
+
+from snakeshell.parser import parse
+from snakeshell.tree import CommandNode
+
+
+def is_command_node(obj):
+    return type(obj) == CommandNode
+
+
+def node_to_dict(obj):
+    return {
+        'exec': obj.execute_path,
+        'args': obj.arguments,
+    }
+
+
+def test_without_arguments():
+    command = '/bin/echo'
+
+    node = parse(command)
+    node_as_dict = node_to_dict(node)
+
+    assert is_command_node(node)
+    assert node_as_dict == {
+        'exec': '/bin/echo',
+        'args': ['/bin/echo'],
+    }
+
+
+def test_with_arguments():
+    command = '/bin/echo a b c'
+
+    node = parse(command)
+    node_as_dict = node_to_dict(node)
+
+    assert is_command_node(node)
+    assert node_as_dict == {
+        'exec': '/bin/echo',
+        'args': ['/bin/echo', 'a', 'b', 'c'],
+    }
+
+
+def test_with_single_quoted_argument():
+    command = "/bin/echo 'a b c'"
+
+    node = parse(command)
+    node_as_dict = node_to_dict(node)
+
+    assert is_command_node(node)
+    assert node_as_dict == {
+        'exec': '/bin/echo',
+        'args': ['/bin/echo', 'a b c'],
+    }
+
+
+def test_with_double_quoted_argument():
+    command = '/bin/echo "a b c"'
+
+    node = parse(command)
+    node_as_dict = node_to_dict(node)
+
+    assert is_command_node(node)
+    assert node_as_dict == {
+        'exec': '/bin/echo',
+        'args': ['/bin/echo', 'a b c'],
+    }
+
+
+def test_with_double_and_single_quoted_arguments():
+    command = '/bin/echo "first one" \'second one\''
+
+    node = parse(command)
+    node_as_dict = node_to_dict(node)
+
+    assert is_command_node(node)
+    assert node_as_dict == {
+        'exec': '/bin/echo',
+        'args': ['/bin/echo', 'first one', 'second one'],
+    }
+
