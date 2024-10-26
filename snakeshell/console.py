a5de5153ad4cd9f7bc2d764352d6f5778ff038ba
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-10-26 23:08:13 +0300

    ✨ Add cursor management support to prompt function

--- snakeshell/console.py
+++ snakeshell/console.py
@@ -1,10 +1,17 @@
 import os
 import sys
+from enum import Enum
 
 
 OR_CONTINUE = '||\n'
 AND_CONTINUE = '&&\n'
 BACKSLASH_CONTINUE = '\\\n'
+
+
+class CursorType(Enum):
+    THICK = '\x1b[5 q'
+    THIN = '\x1b[3 q'
+    DEFAULT = '\x1b[0 q'
 
 
 def write(data: str) -> None:
@@ -37,6 +44,14 @@ def error(msg: str) -> None:
     os.write(2, color_msg.encode('utf-8'))
 
 
+def set_cursor(cursor_type: CursorType) -> None:
+    """
+    Sets the cursor style in the console to the specified type.
+    """
+    change_cursor_escape_sequence = str(cursor_type.value)
+    write(change_cursor_escape_sequence)
+
+
 def prompt_msg() -> str:
     """
     Generate a prompt message indicating the current working directory.
@@ -53,9 +68,13 @@ def prompt() -> str:
     Displays a prompt message to the user and requests
     their input, returning the user's input line.
     """
-    command = ''
     write(prompt_msg())
-    while True:
+    set_cursor(CursorType.THICK)
+
+    command = ''
+    completed = False
+
+    while not completed:
         line = readline()
         command += line
         if command.endswith(BACKSLASH_CONTINUE):
@@ -67,4 +86,7 @@ def prompt() -> str:
             command = command[:-1]
             command += ' '
             continue
-        return command
+        completed = True
+
+    set_cursor(CursorType.DEFAULT)
+    return command
