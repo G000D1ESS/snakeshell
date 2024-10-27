d6edd2c992744fd3b25d81aedc02e2fc5607f63e
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-10-28 01:03:47 +0200

    ♻️ Refactor stream module: remove magic numbers

--- snakeshell/console/stream.py
+++ snakeshell/console/stream.py
@@ -2,12 +2,17 @@ import os
 import sys
 
 
+STDIN = 0
+STDOUT = 1
+STDERR = 2
+
+
 def write(data: str) -> None:
     """
     Writes text to the console without a newline at the end.
     """
     if sys.stdin.isatty():
-        os.write(1, data.encode('utf-8'))
+        os.write(STDOUT, data.encode('utf-8'))
 
 
 def writeline(data: str) -> None:
@@ -21,7 +26,7 @@ def readline() -> str:
     """
     Read a line of text from the console.
     """
-    return os.read(0, 1024).decode('utf-8')
+    return os.read(STDIN, 1024).decode('utf-8')
 
 
 def error(msg: str) -> None:
@@ -29,4 +34,4 @@ def error(msg: str) -> None:
     Writes an error message to the standard error output.
     """
     color_msg = '\033[31msnake: ' + msg + '\033[0m\n'
-    os.write(2, color_msg.encode('utf-8'))
+    os.write(STDERR, color_msg.encode('utf-8'))
