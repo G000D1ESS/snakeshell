15bad7ade5fdc83c30dd29163160bf51cb860cab
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2024-10-27 16:10:17 +0200

    🚧 Add cursor movement support in command line

--- snakeshell/console.py
+++ snakeshell/console.py
@@ -1,11 +1,15 @@
 import os
 import sys
+import termios
+import tty
 from enum import Enum
 
 
 OR_CONTINUE = '||\n'
 AND_CONTINUE = '&&\n'
 BACKSLASH_CONTINUE = '\\\n'
+
+EOF = ''
 
 
 class CursorType(Enum):
@@ -34,6 +38,92 @@ def readline() -> str:
     Read a line of text from the console.
     """
     return os.read(0, 1024).decode('utf-8')
+
+
+def move_cursor(x: int) -> None:
+    """
+    TODO: Write.
+    """
+    delay()
+    if x > 0:
+        os.write(1, f'\x1b[{x}C'.encode())
+    elif x < 0:
+        os.write(1, f'\x1b[{-x}D'.encode())
+
+
+def delay():
+    import time
+    time.sleep(0)
+
+
+def command_line() -> str:
+    """
+    TODO: Write.
+    """
+    default_settings = termios.tcgetattr(0)
+
+    try:
+        tty.setraw(0)
+
+        position = 0
+        buffer = bytearray()
+
+        while True:
+            match ch := os.read(0, 1):
+                # EOF
+                case b'':
+                    return EOF
+
+                # Enter
+                case b'\r':
+                    os.write(1, b'\r\n')
+                    buffer.append(ord(b'\n'))
+                    return buffer.decode('utf-8')
+
+                # Ctrl-D
+                case b'\x04':
+                    if not buffer:
+                        return EOF
+
+                # Ctrl-C
+                case b'\x03':
+                    os.write(0, b'\r\n')
+                    return '\n'
+
+                # Backspace
+                case b'\x7f':
+                    if position > 0:
+                        position -= 1
+                        move_cursor(-1)
+                        del buffer[position]
+
+                # Escape
+                case b'\x1b':
+                    seq = os.read(0, 2).decode()
+                    # Left arrow
+                    if seq == r'[D':
+                        if position > 0:
+                            position -= 1
+                            move_cursor(-1)
+                    # Right arrow
+                    elif seq == r'[C':
+                        if position < len(buffer):
+                            position += 1
+                            move_cursor(+1)
+
+                # Symbols
+                case _:
+                    buffer.insert(position, ord(ch))
+                    position += 1
+                    os.write(1, ch)
+
+            # Update command line
+            move_cursor(-position)
+            os.write(1, '\x1b[K'.encode())
+            os.write(1, buffer)
+            move_cursor(position-len(buffer))
+    finally:
+        termios.tcsetattr(0, termios.TCSADRAIN, default_settings)
 
 
 def error(msg: str) -> None:
@@ -75,7 +165,7 @@ def prompt() -> str:
     completed = False
 
     while not completed:
-        line = readline()
+        line = command_line()
         command += line
         if command.endswith(BACKSLASH_CONTINUE):
             write('> ')
